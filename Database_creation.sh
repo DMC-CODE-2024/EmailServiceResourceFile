7d97ae7b113716bd0729234e73645bea8d69fe56
DMC <surendra.singh20.in@gmail.com>
2024-11-25 09:34:33 +0700

    Update Database_creation.sh

--- Database_creation.sh
+++ Database_creation.sh
@@ -5,18 +5,18 @@ dbDecryptPassword=$(java -jar  ${pass_dypt} dbEncyptPassword)
 
 mysql  -h$dbIp -P$dbPort -u$dbUsername -p${dbDecryptPassword} $appdbName <<EOFMYSQL
 
-insert ignore into sys_param(tag, type, value, feature_name , DESCRIPTION,TYPE,ACTIVE,remark,user_type,modified_by ) VALUES 
-('email_host', 1, 'smtp.office365.com', 'EmailProcess' ,'host name of email server',0,1,'','',''), 
-('email_port', 1, '587', 'EmailProcess','port  of email server',0,1,'','',''), 
-('email_username', 1, 'support@goldilocks-tech.com', 'EmailProcess','username   of email server',0,1,'','',''), 
-('email_password', 1, 'P@ss1MustChange', 'EmailProcess','password  of email server',0,1,'','',''), 
-('email_tps', 1, '5000', 'EmailProcess','tps required for  email server',0,1,'','',''), 
-('email_sleep_timer', 1, '20', 'EmailProcess','time gap in between email to be send ',0,1,'','',''), 
-('email_retry_gap', 1, '5000', 'EmailProcess',' gap after which email retry happen',0,1,'','',''), 
-('email_max_retry_count', 1, '3', 'EmailProcess','retry count for email send',0,1,'','',''), 
-('email_sleep_time', 1, '30', 'EmailProcess','time gap when new batch for email execute',0,1,'','',''), 
-('email_from', 1, 'Info<support@goldilocks-tech.com>', 'EmailProcess','email from sender ',0,1,'','',''),  
-('systemDefaultLanguage', 1, 'en', 'General' ,'default system language',0,1,'','',''); 
+insert ignore into sys_param(tag,  value, feature_name , DESCRIPTION,TYPE,ACTIVE,remark,user_type,modified_by ) VALUES 
+('email_host',  'smtp.office365.com', 'EmailProcess' ,'host name of email server',0,1,'','',''), 
+('email_port',  '587', 'EmailProcess','port  of email server',0,1,'','',''), 
+('email_username',  'support@goldilocks-tech.com', 'EmailProcess','username   of email server',0,1,'','',''), 
+('email_password',  'P@ss1MustChange', 'EmailProcess','password  of email server',0,1,'','',''), 
+('email_tps',  '5000', 'EmailProcess','tps required for  email server',0,1,'','',''), 
+('email_sleep_timer', '20', 'EmailProcess','time gap in between email to be send ',0,1,'','',''), 
+('email_retry_gap',  '5000', 'EmailProcess',' gap after which email retry happen',0,1,'','',''), 
+('email_max_retry_count',  '3', 'EmailProcess','retry count for email send',0,1,'','',''), 
+('email_sleep_time',  '30', 'EmailProcess','time gap when new batch for email execute',0,1,'','',''), 
+('email_from',  'Info<support@goldilocks-tech.com>', 'EmailProcess','email from sender ',0,1,'','',''),  
+('systemDefaultLanguage',  'en', 'General' ,'default system language',0,1,'','',''); 
 
 INSERT ignore INTO eirs_response_param (tag, value, feature_name, language) VALUES ('mail_signature', 'Regards,\nEIRS Team.\nPlease do not reply on this mail', 'EmailProcess', 'en'); 
 
